8704a7ecd33908eccb0e7657b61899052645cf19
larsligthart <mespirtlol@gmail.com>
2013-11-20 13:59:46 +0100

    menu + button

---
+++ button.cpp
@@ -0,0 +1,16 @@
+#include "button.h"
+
+
+button::button(void)
+{
+}
+
+
+button::~button(void)
+{
+}
+
+void button::onClick(){
+
+
+}
---
+++ button.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string.h>
+
+
+class button
+{
+public:
+	button(void);
+	~button(void);
+
+	//string name;
+
+	void onClick();
+
+
+};
+
