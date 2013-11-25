d13ecb5514c40fc0d0a4ee8d33981a204f35c8f3
larsligthart <mespirtlol@gmail.com>
2013-11-25 11:02:28 +0100

    step 1 ray's

--- src/Scene.h
+++ src/Scene.h
@@ -1,12 +1,15 @@
 #pragma once
 
+#include <ISceneCollisionManager.h>
+
+
 class Scene
 {
 public:
 	Scene(void);
 	~Scene(void);
 
-	virtual void Click(int x, int  y);
+	virtual void Click();
 	virtual void start();
 	virtual void update();
 	virtual void render();
