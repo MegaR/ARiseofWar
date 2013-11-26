44de148b9dd4421656f989e2464372dfd617a3bb
Ronald <raalders28@gmail.com>
2013-11-26 11:05:54 +0100

    moved findpath from tilesystem to gamescene

--- src/GameScene.h
+++ src/GameScene.h
@@ -18,11 +18,19 @@ public:
 	std::vector<Entity> entities;
 	int turnCount;
 
-	void turn();
+	void nextTurn();
 	void update();
 	void updateMouse();
 	void mouseRay();
+	std::vector<vector2d<int>>* findPath(vector2d<int> start, vector2d<int> end);
 	
 private:
 	void moveCamera(float x, float y, float z);
+
+	//pathfinding
+	int findElement(std::vector<vector2d<int>> list, vector2d<int> element); 
+	vector2d<int> lowest(int list[][MAPSIZE]);
+	std::vector<vector2d<int>>* get_neighbors(vector2d<int> current);
+	int heuristic_cost_estimate(vector2d<int> current, vector2d<int> goal);
+	std::vector<vector2d<int>>* reconstructed_path(vector2d<int> came_from[][MAPSIZE], vector2d<int> end, vector2d<int> start);
 };
--- src/TileSystem.h
+++ src/TileSystem.h
@@ -13,17 +13,7 @@ class TileSystem
 public:
 	TileSystem(void);
 	~TileSystem(void);
-	std::vector<vector2d<int>>* findPath(vector2d<int> start, vector2d<int> end);
 
 	Tile* tiles[MAPSIZE][MAPSIZE];
-
-private:
-
-	//pathfinding
-	int findElement(std::vector<vector2d<int>> list, vector2d<int> element); 
-	vector2d<int> lowest(int list[][MAPSIZE]);
-	std::vector<vector2d<int>>* get_neighbors(vector2d<int> current);
-	int heuristic_cost_estimate(vector2d<int> current, vector2d<int> goal);
-	std::vector<vector2d<int>>* reconstructed_path(vector2d<int> came_from[][MAPSIZE], vector2d<int> end, vector2d<int> start);
 };
 
