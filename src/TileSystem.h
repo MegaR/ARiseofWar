f6095b562062cd7c0897d5dd50df16795d8ff588
Ronald <raalders28@gmail.com>
2013-11-26 01:01:21 +0100

    pathfinding

--- src/TileSystem.h
+++ src/TileSystem.h
@@ -2,6 +2,7 @@
 #include "Tile.h"
 #include "TileGrass.h"
 
+#include <vector>
 #include <iostream>
 using namespace std;
 
@@ -12,7 +13,17 @@ class TileSystem
 public:
 	TileSystem(void);
 	~TileSystem(void);
+	std::vector<vector2d<int>>* findPath(vector2d<int> start, vector2d<int> end);
 
 	Tile* tiles[MAPSIZE][MAPSIZE];
+
+private:
+
+	//pathfinding
+	int findElement(std::vector<vector2d<int>> list, vector2d<int> element); 
+	vector2d<int> lowest(int list[][MAPSIZE]);
+	std::vector<vector2d<int>>* get_neighbors(vector2d<int> current);
+	int heuristic_cost_estimate(vector2d<int> current, vector2d<int> goal);
+	std::vector<vector2d<int>>* reconstructed_path(vector2d<int> came_from[][MAPSIZE], vector2d<int> end, vector2d<int> start);
 };
 
