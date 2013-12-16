d7266652b45ed20aba098a177e3359f4c8bd637e
Mei <cirno@live.nl>
2013-12-16 14:33:03 +0100

    Peasant class added

---
+++ UnitPeasant.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Unit.h"
+
+class UnitPeasant: public Unit
+{
+public:
+	UnitPeasant(int _x, int _y, int _player);
+	~UnitPeasant();
+};
+
---
+++ src/UnitPeasant.cpp
@@ -0,0 +1,30 @@
+#include "UnitPeasant.h"
+
+#include "Game.h"
+
+#include <iostream>
+using namespace std;
+
+UnitPeasant::UnitPeasant(int _x, int _y, int _player) : Unit(_x, _y, _player)
+{
+	hp = 1;
+	attack = 1;
+	defense = 1;
+	maxDistance = 7;
+	attackDistance = 1;
+	
+	Game* game = &Game::getInstance();
+	model = game->sceneManager->getMesh("res/unitPeasant.md2");
+	if(player == 0) {
+		texture = game->videoDriver->getTexture("res/unitPeasant.png");
+	} else {
+		texture = game->videoDriver->getTexture("res/unitPeasantEnemy.png");
+	}
+	
+	addModel();
+}
+
+
+UnitPeasant::~UnitPeasant()
+{
+}
