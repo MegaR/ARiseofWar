fe99b94f5d222c86baec31c17e3497b22aa5877b
Ronald <raalders28@gmail.com>
2014-01-20 09:59:02 +0100

    lumbermill uses and creates resources

--- src/Lumbermill.h
+++ src/Lumbermill.h
@@ -1,12 +1,14 @@
 #pragma once
 #include "Building.h"
 
-
+#define LUMBERMILLCOST 10, 0, 0, 0
+#define LUMBERMILLINCREASE -10, 0, 0, 0
 
 class Lumbermill : public Building
 {
 public:
 	Lumbermill(int tileX, int tileY, int player, Scene* scene);
 	~Lumbermill(void);
+	virtual void startTurn();
 };
 
